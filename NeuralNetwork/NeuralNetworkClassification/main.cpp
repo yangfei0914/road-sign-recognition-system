093e763ab6688e3e6764bf8b552d26515e32df60
Thai.Nguyen18 <Thai.Nguyen18@gmail.com>
2012-12-06 03:46:34 +0000

    Example code copied from machinelearning.pdf; Shows lots of machine learning algorithms

--- NeuralNetwork/NeuralNetworkClassification/main.cpp
+++ NeuralNetwork/NeuralNetworkClassification/main.cpp
@@ -4,7 +4,5 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
-}
 
+}
---
+++ NeuralNetwork/NeuralNetworkClassification/roadsignclassifier.cpp
@@ -0,0 +1,11 @@
+#include "roadsignclassifier.h"
+
+RoadSignClassifier::RoadSignClassifier()
+{
+}
+
+RoadSignClassifier::RoadSignClassifier(double inputs[], int outputs)
+{
+    //TODO: Implement road sign classifier based on ANN (mlp specifically).
+    //TODO: If MLP doesn't produce accurate results then use SVM
+}
